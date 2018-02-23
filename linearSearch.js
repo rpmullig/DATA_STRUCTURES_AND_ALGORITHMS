69531d6c5245648b3301a1c5f618008f1f17fd12
Bobby Mulligan <robertpmulligan@gmail.com>
2018-02-23 11:02:01 -0600

    Create linearSearch.js
    Linear Search from CLRS Intro to Algorithms

---
+++ linearSearch.js
@@ -0,0 +1,22 @@
+/* Linear Search from Intro to Algorithms */ 
+/*
+Author: Robert P Mulligan
+Created: 2/23/2018
+Run-time: N 
+*/ 
+
+
+var a = [4, 1, 2, 20, 2300, 300, 324, 20];
+
+function linearSearch(value, array) {
+ for(var i = 0; i < array.length; i++){
+   // soft equals to array object
+   if(a[i] == value) {return i;}
+ }
+  return -1; 
+}
+
+linearSearch(4, a); // 0
+linearSearch(300, a); // 5
+linearSearch(20, a); // 3
+linearSearch(2039, a); // -1
