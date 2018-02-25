36c4342a111007c62e69c5db652582ff45fb696c
Bobby Mulligan <robertpmulligan@gmail.com>
2018-02-25 12:10:58 -0600

    Update kadaneAlgorithm.js

--- Intro_to_Algorithms_CLRS/kadaneAlgorithm.js
+++ Intro_to_Algorithms_CLRS/kadaneAlgorithm.js
@@ -1,4 +1,17 @@
+/* Kadane's Algorithm from Intro to Algorithms */ 
+/*
+Author: Robert P Mulligan
+Created: 2/25/2018
 
+RAM Computational Model:
+	-n is the numbers in the array
+	-O(n)
+
+Maximum subarray problem:
+What is the maximum subarray sum that you can achieve from a given array?
+
+
+*/
 var arr = [ 1, 3, 4, -6, 11, -10, 100, -1, -2, -3, 104, 100, -100]; 
 
 function kadane(a){
