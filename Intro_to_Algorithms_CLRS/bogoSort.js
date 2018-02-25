e540720386ad24bfe61465c19084e05bb0cbfed6
Bobby Mulligan <robertpmulligan@gmail.com>
2018-02-24 21:42:29 -0600

    Create bogoSort.js

---
+++ Intro_to_Algorithms_CLRS/bogoSort.js
@@ -0,0 +1,52 @@
+/* Bogo Sort */ 
+/*
+Author: Robert P Mulligan
+Created: 2/24/2018
+
+RAM Computational Model:
+    -n is the numbers in the array
+    -O((n+1)!)
+    -highly inefficient, used for testing purposes only
+*/
+
+var a = [12, 100, 30, -10, 1, 3, 501645, -150, 564, 1, 13];
+
+function bogoSort(a){
+ 
+ var new_array = []; 
+  a.forEach(function(item) {
+    new_array.push(item)
+  });
+   
+  function notSorted(arr){
+    for(var x = 1; x < arr.length; x++) {
+     if(arr[x-1] > arr[x]){return true; } 
+    }
+    return false; 
+  }
+ 
+ function shuffle(arr){
+    var max = arr.length, random;
+    for(var i = 0; i < max; i++){
+    random = Math.floor(Math.random() * max); 
+    swap(arr, i, random)
+    }
+  return arr;  
+ }
+ 
+ function swap(arr, i, x) {
+    var temp = arr[i];
+    arr[i] = arr[x];
+    arr[x] = temp; 
+    return arr; 
+ }
+ 
+ 
+ while(notSorted(new_array)) {
+    shuffle(new_array)  
+  }  
+  
+  return new_array;
+}
+
+bogoSort(a); 
